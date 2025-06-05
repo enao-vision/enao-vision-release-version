03c31e184943c37a4c32479851e1c3f77b4e40fc
Ferdinand von den Eichen <ferdinand@vondeneichen.email>
2025-06-05 14:33:33 +0200

    Add central release sh

---
+++ version.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+# --- Configuration ---
+MAJOR=1
+MINOR=2
+PATCH=5
+
+# --- Generate Date ---
+DATE=$(date +%d-%m-%Y)
+
+# --- Output version string ---
+echo "${MAJOR}.${MINOR}.${PATCH}+${DATE}"
